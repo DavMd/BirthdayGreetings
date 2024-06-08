c09817ee1f7110a286cc2ae02fe05e1a431fc768
Maxim <counterggg@gmail.com>
2024-06-08 16:55:49 +0300

    Updated User model

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,8 +3,9 @@ package models
 import "time"
 
 type User struct {
-	ID       int64     `json:"id" db:"id"`
-	Username string    `json:"username" db:"username"`
-	Email    string    `json:"email" db:"email"`
-	Birthday time.Time `json:"birthday" db:"birthday"`
+	ID         int64     `json:"id" db:"id"`
+	Username   string    `json:"username" db:"username"`
+	Password   string    `json:"password" db:"password"`
+	TelegramID int64     `json:"telegram_id" db:"telegram_id"`
+	Birthday   time.Time `json:"birthday" db:"birthday"`
 }
